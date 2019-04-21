cab6eb7aac209bb5565e78ef9789c482e81b075c
Santhanu Venugopal Sunitha <ssunitha3@gatech.edu>
2019-04-20 20:51:58 -0400

    updated negatives

--- Model/create_negatives.py
+++ Model/create_negatives.py
@@ -13,6 +13,7 @@ train_count = 45000
 test_count  = 2000
 valid_count = 500
 window = (32,32)
+crop_out = (200,200)
 
 if not os.path.exists(dest_path_train):
     os.mkdir(dest_path_train)
@@ -27,10 +28,11 @@ image_count = 0
 for images in negative_images:
 
     img = cv2.imread(image_path+images)
-    for row in range(0, img.shape[0] - window[0]):
-        for col in range(0, img.shape[1] - window[1]):
+    for row in range(0, img.shape[0] - crop_out[0]):
+        for col in range(0, img.shape[1] - crop_out[1]):
 
-            crop = img[row:row + 32, col:col + 32]
+            crop = img[row:row + crop_out[0], col:col + crop_out[1]]
+            crop = cv2.resize(crop,window)
             image_count +=1
             if image_count <= train_count:
                 cv2.imwrite(dest_path_train + "{}.png".format(image_count),crop)
